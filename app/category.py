dd01c68f5ed21ca263a18fab759439f78dc1fb98
kaiquemoreira <kaique.moreira@truckpad.com.br>
2024-09-24 15:22:03 -0300

    using dataclass for Category entity

--- app/category.py
+++ app/category.py
@@ -1,12 +1,31 @@
-from uuid import uuid4, UUID
+from dataclasses import dataclass, field
+from uuid import UUID, uuid4
 
 
+@dataclass
 class Category:
-    def __init__(self, name: str, _id: UUID = uuid4(), description: str = None, is_active: bool = True):
-        self.id = _id
-        self.name = name
-        self.description = description
-        self.is_active = is_active
+    name: str
+    description: str = None
+    is_active: bool = True
+    id: UUID = field(default_factory=uuid4)
 
+    def __post_init__(self):
+        self.validate()
+
+    def validate(self):
         if len(self.name) > 255:
             raise ValueError("name must have less than 256 characters")
+
+        if not self.name:
+            raise ValueError("name can't be empty")
+
+    def update(self, name: str, description: str):
+        self.name = name
+        self.description = description
+
+        self.validate()
+
+    def activate(self):
+        self.is_active = True
+
+        self.validate()
