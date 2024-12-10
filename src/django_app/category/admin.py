cf7b0147e6f0453923c03c50aca7170e579d4473
kaiquemoreira <kaique.moreira@truckpad.com.br>
2024-12-10 20:52:59 -0300

    feat: category admin register

--- src/django_app/category/admin.py
+++ src/django_app/category/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from django_app.category.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
--- src/django_app/settings.py
+++ src/django_app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "django_app.category",
+    "src.django_app.category",
 ]
 
 MIDDLEWARE = [
